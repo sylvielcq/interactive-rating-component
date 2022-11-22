f26560ec1da674c0239cd52b97baf9e342436ac0
sylvielcq <slcq.dev@gmail.com>
2022-11-22 14:55:45 -0800

    Add JS

--- state-change.js
+++ state-change.js
@@ -1,8 +1,21 @@
+// Add class to selected rating
+const ratings = document.getElementsByClassName("ratings");
+
+ratings.onclick = function() {
+  
+};
+
+// Trigger state change
+
 const ratingComp = document.getElementById("rating-comp");
 const thankYouComp = document.getElementById("thank-you-comp");
 const btn = document.getElementById("submit");
 
-btn.onclick = function () {
+btn.onclick = function() {
   ratingComp.style.display = "none";
   thankYouComp.style.display = "block";
 };
+
+// Save rating
+
+const selectedRating = document
