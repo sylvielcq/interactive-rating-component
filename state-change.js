61c7ebeff496c3a1a69306028809834d10664325
sylvielcq <slcq.dev@gmail.com>
2022-12-01 11:33:35 -0800

    Add hover effect on submit btn. Add JS for FormData retrieval.

--- state-change.js
+++ state-change.js
@@ -1,9 +1,46 @@
-// Add class to selected rating
-const ratings = document.getElementsByClassName("ratings");
 
-ratings.onclick = function() {
+// Get rating
+
+// const ratings = document.getElementsByClassName("ratings");
+
+// ratings.onclick = function() {
+//   // const selectedRating = ratings.children.value;
+//   // console.log(selectedRating);
+
+// };
+
+const form = document.querySelector("form");
+const log = document.querySelector("#log");
+
+form.addEventListener("submit", (event) => {
   
-};
+  event.preventDefault();
+
+  const data = new FormData(form);
+  let output = "";
+  for (const entry of data) {
+    output = `${output}${entry[0]}=${entry[1]}`;
+  }
+  console.log(output);
+  log.innerText = output;
+
+  },
+  false
+);
+
+// const radioBtns = document.getElementById("radios");
+
+
+// radioBtns.onclick = function() {
+//   console.log("radio btn clicked");
+//   // console.log(this.value);
+
+// };
+
+
+
+
+// Add class to selected rating
 
 // Trigger state change
 
@@ -18,4 +55,3 @@ btn.onclick = function() {
 
 // Save rating
 
-const selectedRating = document
